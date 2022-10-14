eb2d9210724d171b3b4e8ab8317129fb811b1190
Brian Dainton <brian.dainton@gmail.com>
2022-10-14 09:55:19 -0500

    added link to latest speaking engagement

---
+++ _app/immutable/chunks/2-f40b6e92.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/companion/_layout.svelte-bc98cb9c.js";export{t as component};
---
+++ _app/immutable/components/pages/_page.svelte-29c3d659.js
@@ -0,0 +1,47 @@
+import{S as Ht,i as Kt,s as jt,k as l,a as b,q as a,M as Gt,l as o,h as n,c as k,m as s,r,n as t,J as Jt,F as e,b as Ut,A as ht}from"../../chunks/index-6ebe39d3.js";function Ot(Wt){let v,y,O,re,_,R,E,x,w,dt,ie,Y,ce,he,i,m,z,de,ue,Q,fe,ge,I,pe,me,_e,P,be,X,ke,ve,Ee,T,we,C,Ae,ye,xe,d,Z,Ie,Pe,D,Te,Ce,B,De,Be,S,Se,Me,M,Ne,qe,Le,c,$,Ve,Fe,N,Ue,We,q,He,Ke,L,je,Ge,V,Je,Oe,F,Re,Ye,ze,h,ee,Qe,Xe,U,Ze,$e,W,et,tt,H,at,rt,K,nt,lt,j,ot,st,it,te,ae,ct;return{c(){v=l("link"),y=l("link"),O=l("link"),re=b(),_=l("main"),R=l("header"),E=l("div"),x=l("a"),w=l("img"),ie=b(),Y=l("h1"),ce=a("Brian Dainton"),he=b(),i=l("div"),m=l("p"),z=l("span"),de=a("Today"),ue=a(`, I'm an
+      `),Q=l("a"),fe=a("engineering leadership coach"),ge=a(`, startup advisor, mentor, and
+      speaker (most recently on
+      `),I=l("a"),pe=a("The Drunk Web"),me=a(" podcast)."),_e=b(),P=l("p"),be=a("I created and maintain "),X=l("a"),ke=a("The Engineering Manager's Companion"),ve=a(` to orient,
+      aid, and accelerate new software engineering leaders.`),Ee=b(),T=l("p"),we=a("I also run "),C=l("a"),Ae=a("Verkout"),ye=a(`, the quick daily workout
+      challenge.`),xe=b(),d=l("p"),Z=l("span"),Ie=a("Built a life"),Pe=a(` as a
+      `),D=l("a"),Te=a("tech founder x 4"),Ce=a(`
+      and a long-time
+      `),B=l("a"),De=a("engineering leader"),Be=a(`. Won an
+      `),S=l("a"),Se=a("Emmy Award"),Me=a(`
+      along the way. Born and raised in Chicago. Computer Science at
+      `),M=l("a"),Ne=a("UIUC"),qe=a(`. Married and living in Austin.
+      Trying to be a good husband and dad.`),Le=b(),c=l("p"),$=l("span"),Ve=a("For fun"),Fe=a(`, I
+      `),N=l("a"),Ue=a("golf"),We=a(`, gamble,
+      `),q=l("a"),He=a("read"),Ke=a(`,
+      stay fit via the `),L=l("a"),je=a("Daily Verkout"),Ge=a(`, and build
+      things like `),V=l("a"),Je=a("AirFryMe"),Oe=a(` and
+      `),F=l("a"),Re=a("Tulaga"),Ye=a("."),ze=b(),h=l("p"),ee=l("span"),Qe=a("Please follow me"),Xe=a(` on
+      `),U=l("a"),Ze=a("TikTok"),$e=a(`,
+      `),W=l("a"),et=a("YouTube"),tt=a(`,
+      `),H=l("a"),at=a("Instagram"),rt=a(`, and
+      `),K=l("a"),nt=a("Twitter"),lt=a(`. Connect on
+      `),j=l("a"),ot=a("LinkedIn"),st=a(`. Otherwise,
+      brian.dainton (gmail).`),it=b(),te=l("p"),ae=l("a"),ct=a("\u{1F91C}\u{1F4A5}\u{1F91B}"),this.h()},l(A){const G=Gt('[data-svelte="svelte-m1sm24"]',document.head);v=o(G,"LINK",{rel:!0,href:!0}),y=o(G,"LINK",{rel:!0,href:!0,crossorigin:!0}),O=o(G,"LINK",{href:!0,rel:!0}),G.forEach(n),re=k(A),_=o(A,"MAIN",{class:!0});var ne=s(_);R=o(ne,"HEADER",{class:!0});var ut=s(R);E=o(ut,"DIV",{class:!0});var le=s(E);x=o(le,"A",{href:!0,target:!0});var ft=s(x);w=o(ft,"IMG",{class:!0,src:!0,alt:!0,title:!0}),ft.forEach(n),ie=k(le),Y=o(le,"H1",{class:!0});var gt=s(Y);ce=r(gt,"Brian Dainton"),gt.forEach(n),le.forEach(n),ut.forEach(n),he=k(ne),i=o(ne,"DIV",{class:!0});var u=s(i);m=o(u,"P",{});var J=s(m);z=o(J,"SPAN",{class:!0});var pt=s(z);de=r(pt,"Today"),pt.forEach(n),ue=r(J,`, I'm an
+      `),Q=o(J,"A",{href:!0});var mt=s(Q);fe=r(mt,"engineering leadership coach"),mt.forEach(n),ge=r(J,`, startup advisor, mentor, and
+      speaker (most recently on
+      `),I=o(J,"A",{href:!0,target:!0});var _t=s(I);pe=r(_t,"The Drunk Web"),_t.forEach(n),me=r(J," podcast)."),J.forEach(n),_e=k(u),P=o(u,"P",{});var oe=s(P);be=r(oe,"I created and maintain "),X=o(oe,"A",{href:!0});var bt=s(X);ke=r(bt,"The Engineering Manager's Companion"),bt.forEach(n),ve=r(oe,` to orient,
+      aid, and accelerate new software engineering leaders.`),oe.forEach(n),Ee=k(u),T=o(u,"P",{});var se=s(T);we=r(se,"I also run "),C=o(se,"A",{href:!0,target:!0});var kt=s(C);Ae=r(kt,"Verkout"),kt.forEach(n),ye=r(se,`, the quick daily workout
+      challenge.`),se.forEach(n),xe=k(u),d=o(u,"P",{class:!0});var p=s(d);Z=o(p,"SPAN",{class:!0});var vt=s(Z);Ie=r(vt,"Built a life"),vt.forEach(n),Pe=r(p,` as a
+      `),D=o(p,"A",{href:!0,target:!0});var Et=s(D);Te=r(Et,"tech founder x 4"),Et.forEach(n),Ce=r(p,`
+      and a long-time
+      `),B=o(p,"A",{href:!0,target:!0});var wt=s(B);De=r(wt,"engineering leader"),wt.forEach(n),Be=r(p,`. Won an
+      `),S=o(p,"A",{href:!0,target:!0});var At=s(S);Se=r(At,"Emmy Award"),At.forEach(n),Me=r(p,`
+      along the way. Born and raised in Chicago. Computer Science at
+      `),M=o(p,"A",{href:!0,target:!0});var yt=s(M);Ne=r(yt,"UIUC"),yt.forEach(n),qe=r(p,`. Married and living in Austin.
+      Trying to be a good husband and dad.`),p.forEach(n),Le=k(u),c=o(u,"P",{class:!0});var f=s(c);$=o(f,"SPAN",{class:!0});var xt=s($);Ve=r(xt,"For fun"),xt.forEach(n),Fe=r(f,`, I
+      `),N=o(f,"A",{href:!0,target:!0});var It=s(N);Ue=r(It,"golf"),It.forEach(n),We=r(f,`, gamble,
+      `),q=o(f,"A",{href:!0,target:!0});var Pt=s(q);He=r(Pt,"read"),Pt.forEach(n),Ke=r(f,`,
+      stay fit via the `),L=o(f,"A",{href:!0,target:!0});var Tt=s(L);je=r(Tt,"Daily Verkout"),Tt.forEach(n),Ge=r(f,`, and build
+      things like `),V=o(f,"A",{href:!0,target:!0});var Ct=s(V);Je=r(Ct,"AirFryMe"),Ct.forEach(n),Oe=r(f,` and
+      `),F=o(f,"A",{href:!0,target:!0});var Dt=s(F);Re=r(Dt,"Tulaga"),Dt.forEach(n),Ye=r(f,"."),f.forEach(n),ze=k(u),h=o(u,"P",{class:!0});var g=s(h);ee=o(g,"SPAN",{class:!0});var Bt=s(ee);Qe=r(Bt,"Please follow me"),Bt.forEach(n),Xe=r(g,` on
+      `),U=o(g,"A",{href:!0,target:!0});var St=s(U);Ze=r(St,"TikTok"),St.forEach(n),$e=r(g,`,
+      `),W=o(g,"A",{href:!0,target:!0});var Mt=s(W);et=r(Mt,"YouTube"),Mt.forEach(n),tt=r(g,`,
+      `),H=o(g,"A",{href:!0,target:!0});var Nt=s(H);at=r(Nt,"Instagram"),Nt.forEach(n),rt=r(g,`, and
+      `),K=o(g,"A",{href:!0,target:!0});var qt=s(K);nt=r(qt,"Twitter"),qt.forEach(n),lt=r(g,`. Connect on
+      `),j=o(g,"A",{href:!0,target:!0});var Lt=s(j);ot=r(Lt,"LinkedIn"),Lt.forEach(n),st=r(g,`. Otherwise,
+      brian.dainton (gmail).`),g.forEach(n),it=k(u),te=o(u,"P",{class:!0});var Vt=s(te);ae=o(Vt,"A",{href:!0});var Ft=s(ae);ct=r(Ft,"\u{1F91C}\u{1F4A5}\u{1F91B}"),Ft.forEach(n),Vt.forEach(n),u.forEach(n),ne.forEach(n),this.h()},h(){document.title="Brian Dainton, Software Engineering Leadership Coach",t(v,"rel","preconnect"),t(v,"href","https://fonts.googleapis.com"),t(y,"rel","preconnect"),t(y,"href","https://fonts.gstatic.com"),t(y,"crossorigin",""),t(O,"href","https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap"),t(O,"rel","stylesheet"),t(w,"class","rounded-full border border-lightGray p-2 w-48 h-48 m-2 shadow-2xl"),Jt(w.src,dt="/img/brian-color.png")||t(w,"src",dt),t(w,"alt","Brian Dainton Profile Picture"),t(w,"title","Brian Dainton Profile Picture"),t(x,"href","/img/brian-color.png"),t(x,"target","_blank"),t(Y,"class","font-rock-salt font-bold text-red100 text-center text-5xl -mt-8 pb-16"),t(E,"class","flex flex-col content-center items-center"),t(R,"class","max-w-screen-md mx-auto"),t(z,"class","font-bold"),t(Q,"href","/companion/coaching"),t(I,"href","https://www.thedrunkweb.com/episode/45"),t(I,"target","_blank"),t(X,"href","/companion"),t(C,"href","https://verkout.com"),t(C,"target","_blank"),t(Z,"class","font-bold"),t(D,"href","https://linkedin.com/in/bdainton"),t(D,"target","_blank"),t(B,"href","https://linkedin.com/in/bdainton"),t(B,"target","_blank"),t(S,"href","https://www.bizjournals.com/austin/blog/techflash/2015/01/spredfast-nabs-tech-emmy-after-helping-obama-in.html"),t(S,"target","_blank"),t(M,"href","https://cs.illinois.edu"),t(M,"target","_blank"),t(d,"class","pt-8"),t($,"class","font-bold"),t(N,"href","https://thegrint.com/hdcp_lookup/records/758239"),t(N,"target","_blank"),t(q,"href","https://www.goodreads.com/user/show/73589478-brian-dainton"),t(q,"target","_blank"),t(L,"href","https://verkout.com"),t(L,"target","_blank"),t(V,"href","https://airfryme.com"),t(V,"target","_blank"),t(F,"href","https://tulaga.com"),t(F,"target","_blank"),t(c,"class","pt-8"),t(ee,"class","font-bold"),t(U,"href","https://tiktok.com/@bdainton"),t(U,"target","_blank"),t(W,"href","https://www.youtube.com/channel/UCgj_o7A2nikioh0J0ph7HBA"),t(W,"target","_blank"),t(H,"href","https://instagram.com/bdainton"),t(H,"target","_blank"),t(K,"href","https://twitter.com/bdainton"),t(K,"target","_blank"),t(j,"href","https://linkedin.com/in/bdainton"),t(j,"target","_blank"),t(h,"class","pt-8"),t(ae,"href","/"),t(te,"class","pt-16 text-center"),t(i,"class","max-w-screen-md mx-auto text-lg"),t(_,"class","flex flex-col bg-white overflow-auto p-4")},m(A,G){e(document.head,v),e(document.head,y),e(document.head,O),Ut(A,re,G),Ut(A,_,G),e(_,R),e(R,E),e(E,x),e(x,w),e(E,ie),e(E,Y),e(Y,ce),e(_,he),e(_,i),e(i,m),e(m,z),e(z,de),e(m,ue),e(m,Q),e(Q,fe),e(m,ge),e(m,I),e(I,pe),e(m,me),e(i,_e),e(i,P),e(P,be),e(P,X),e(X,ke),e(P,ve),e(i,Ee),e(i,T),e(T,we),e(T,C),e(C,Ae),e(T,ye),e(i,xe),e(i,d),e(d,Z),e(Z,Ie),e(d,Pe),e(d,D),e(D,Te),e(d,Ce),e(d,B),e(B,De),e(d,Be),e(d,S),e(S,Se),e(d,Me),e(d,M),e(M,Ne),e(d,qe),e(i,Le),e(i,c),e(c,$),e($,Ve),e(c,Fe),e(c,N),e(N,Ue),e(c,We),e(c,q),e(q,He),e(c,Ke),e(c,L),e(L,je),e(c,Ge),e(c,V),e(V,Je),e(c,Oe),e(c,F),e(F,Re),e(c,Ye),e(i,ze),e(i,h),e(h,ee),e(ee,Qe),e(h,Xe),e(h,U),e(U,Ze),e(h,$e),e(h,W),e(W,et),e(h,tt),e(h,H),e(H,at),e(h,rt),e(h,K),e(K,nt),e(h,lt),e(h,j),e(j,ot),e(h,st),e(i,it),e(i,te),e(te,ae),e(ae,ct)},p:ht,i:ht,o:ht,d(A){n(v),n(y),n(O),A&&n(re),A&&n(_)}}}class Yt extends Ht{constructor(v){super(),Kt(this,v,null,Ot,jt,{})}}export{Yt as default};
